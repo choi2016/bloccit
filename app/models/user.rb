c780bc6ae82ca597d2e023009941bb5ef144fc9e
Edward Choi <edwardchoi.2011@gmail.com>
2017-10-28 18:50:42 -0700

    CP23 assignment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase if email.present? }
+  before_save {
+  	  #create empty array to store name
+      name_array = []
+      #split all the names, then iterate through them
+      #while capitalizing every first letter and add to name_array
+      name.to_s.split.each do |part|
+          name_array << part.capitalize
+      end
+      #save the capitalized names to the name attribute seperated by space
+      self.name = name_array.join(" ")
+  }
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
 
