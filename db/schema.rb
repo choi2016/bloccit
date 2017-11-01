309226704c60bb34b01ad1ad832b1f3f461bf846
Edward Choi <edwardchoi.2011@gmail.com>
2017-10-31 20:04:54 -0700

    CP27 checkpoint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171031200221) do
+ActiveRecord::Schema.define(version: 20171101015503) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20171031200221) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
   end
 
 end
