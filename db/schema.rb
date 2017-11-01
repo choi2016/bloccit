6a8303e718087ada30fd9989c3e8cc949f3f10f9
Edward Choi <edwardchoi.2011@gmail.com>
2017-11-01 15:32:21 -0700

    CP28 checkpoint

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171101015503) do
+ActiveRecord::Schema.define(version: 20171101204306) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -29,24 +31,6 @@ ActiveRecord::Schema.define(version: 20171101015503) do
     t.integer "user_id"
     t.index ["topic_id"], name: "index_posts_on_topic_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
-  end
-
-  create_table "questions", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.boolean "resolved"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "sponsored_posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.integer "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "topic_id"
-    t.index ["topic_id"], name: "index_sponsored_posts_on_topic_id"
   end
 
   create_table "topics", force: :cascade do |t|
