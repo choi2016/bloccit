4e42ee504752d836d87c9410236e9da0572bdfc6
Edward Choi <edwardchoi.2011@gmail.com>
2017-11-02 19:50:43 -0700

    CP29 checkpoint

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,12 +20,15 @@ end
 topics = Topic.all
 
 50.times do
-  Post.create!(
-	  user:   users.sample,
+  post = Post.create!(	  
+    user:   users.sample,
     topic:  topics.sample,	   	
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
   )
+  
+  post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+  rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
 end
 
 posts = Post.all
