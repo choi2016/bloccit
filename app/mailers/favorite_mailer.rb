450545cf6a8d9993ec2b11ab7359db51d14afc40
Edward Choi <edwardchoi.2011@gmail.com>
2017-11-06 13:18:36 -0800

    CP31 assignment complete

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -3,7 +3,7 @@ class FavoriteMailer < ApplicationMailer
    
     def new_comment(user, post, comment)
  
-        headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
+        headers["Message-ID"] = "<post/#{post.id}@your-app-name.example>"
         headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
         headers["References"] = "<post/#{post.id}@your-app-name.example>"
       	
---
+++ spec/factories/comments.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :comment do
+    body RandomData.random_paragraph
+    user
+    post 
+  end
+end
